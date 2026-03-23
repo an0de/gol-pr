858531aee33cbfb49f193e8994604219a15c0a18
an0de <149331026+an0de@users.noreply.github.com>
2026-03-23 23:40:04 +0100

    change build.sh

--- gol/build.sh
+++ gol/build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
+uv sync
+mkdir -p staticfiles
+uv run python manage.py makemigrations
+uv run python manage.py migrate
+
 cd app || exit 1
 npm ci
 npm run build
 cd - || exit 1
 
-uv sync
 uv run python manage.py collectstatic --noinput
-uv run python manage.py makemigrations
-uv run python manage.py migrate
