04fdab1986236425c6709e1bce03b827c1a9dfd9
an0de <149331026+an0de@users.noreply.github.com>
2026-03-24 03:18:51 +0100

    change vercel.json

--- gol/gol/settings.py
+++ gol/gol/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.static",
             ],
         },
     },
@@ -137,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    ("static", BASE_DIR / "static"),
+    BASE_DIR / "static",
 ]
 
 
